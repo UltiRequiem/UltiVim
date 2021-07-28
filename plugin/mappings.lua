38a9b3c48e39eb434deba73712dac9f449ca631f
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-27 19:23:05 -0500

    :art: More Mappings

--- plugin/mappings.lua
+++ plugin/mappings.lua
@@ -20,23 +20,34 @@ vim.g.mapleader = " "
 -- Escape from insert mode
 mapper("i", "<C-c>", "<Esc>")
 
+-- When I have a new buffer and need to put the name this useful
+mapper("n", "<C-s>", ":w ")
+
 -- Nice
 mapper("n", "ZE", ":qa!<CR>")
 
 -- Duplitcate Line
 mapper("n", "tt", ":t.<CR>")
 
+--  Toggle Numbers
+mapper("n", "<leader>n", ":set nu! rnu!<CR>")
+
+-- Tabs Stuff
+mapper("n", "<S-t>", ":enew<CR>")
+mapper("n", "<C-t>b", ":tabnew<CR>")
+mapper("n", "<S-x>", ":bd!<CR>")
+
+-- use ESC to turn off search highlighting
+mapper("n", "<Esc>", ":noh<CR>")
+
+-- get out of terminal with jk
+mapper("t", "<Esc>", "<C-\\><C-n>")
+
 -- Resize with arrows
 mapper("n", "<C-Up>", ":resize -2<CR>")
 mapper("n", "<C-Down>", ":resize +2<CR>")
 mapper("n", "<C-Left>", ":vertical resize -2<CR>")
 mapper("n", "<C-Right>", ":vertical resize +2<CR>")
-
--- Better window movement
-mapper("n", "<C-h>", "<C-w>h")
-mapper("n", "<C-j>", "<C-w>j")
-mapper("n", "<C-k>", "<C-w>k")
-mapper("n", "<C-l>", "<C-w>l")
 
 -- Copy to OS clipboard.
 mapper('n', '<Leader>y', '"+y')
@@ -55,13 +66,13 @@ mapper('v', '<Leader>P', '"+P"`"`"')
 mapper("n", "<C-F>", ":Telescope live_grep<CR>")
 mapper("n", "<C-P>", ":Telescope find_files<CR>")
 
+-- Tree
+mapper("n", "<C-n>", ":NvimTreeToggle<CR>")
+
 -- Hop.nvim
 mapper("n", "<Leader>f", ":HopWord<CR>")
 mapper("n", "<Leader>l", ":HopLine<CR>")
 mapper("n", "<Leader>o", ":HopPattern<CR>")
-
--- File Tree Explorer
-mapper("n", "<C-n>", ":NvimTreeToggle<CR>")
 
 -- Coc.nvim
 mapper("n", "<Leader>k", ":CocCommand terminal.Toggle<CR>")
