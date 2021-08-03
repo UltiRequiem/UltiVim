b7d021686ed1a095ed57da2522803369d627b023
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-02 22:33:04 -0500

    :art: Update

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,7 +1,11 @@
 vim.cmd [[
   command! -buffer -bar PyRun :!python3.10 %
+  command! -buffer -bar DelComments :g/#/d
 ]]
 
 vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>rp', ':PyRun<CR>',
+                            {noremap = true, expr = false, silent = true})
+
+vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>rd', ':DelComments<CR>',
                             {noremap = true, expr = false, silent = true})
 
--- ftplugin/yaml.lua
+++ ftplugin/yaml.lua
@@ -1,3 +1,8 @@
 vim.cmd [[
   setl ts=2 sts=2 sw=2 et
+  command! -buffer -bar DelComments :g/#/d
 ]]
+
+vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>rd', ':DelComments<CR>',
+                            {noremap = true, expr = false, silent = true})
+
