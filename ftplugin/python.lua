ab3df459fb331d41b997128b58282c48ad58a14b
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-13 15:37:40 -0500

    :sparkles: Poetry

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,5 +1,6 @@
 vim.cmd([[
   command! -buffer -bar PyRun :!python3.10 %
+  command! -buffer -bar PoetryRun :!poetry run python %
 ]])
 
 vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>rp', ':PyRun<CR>', { noremap = true, expr = false, silent = true })
