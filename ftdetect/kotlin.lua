d426094b9d46124d88d354e4195c508ba74816e8
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-20 08:57:17 -0500

    :sparkles: Update

--- ftdetect/kotlin.lua
+++ ftdetect/kotlin.lua
@@ -1,4 +1,4 @@
 vim.cmd([[
-autocmd BufNewFile,BufRead *.kt setfiletype kotlin
-autocmd BufNewFile,BufRead *.kts setfiletype kotlin
+  autocmd BufNewFile,BufRead *.kt setfiletype kotlin
+  autocmd BufNewFile,BufRead *.kts setfiletype kotlin
 ]])
