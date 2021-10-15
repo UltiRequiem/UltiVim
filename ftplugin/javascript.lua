0de19a0cffcc7dbec0f3bd158d52013fc3dbe0dd
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-10-15 15:14:38 -0500

    :bulb: Bad Output

--- ftplugin/javascript.lua
+++ ftplugin/javascript.lua
@@ -2,6 +2,11 @@ vim.cmd([[
   command! -buffer -bar Run :!node %
 ]])
 
+
+-- vim.cmd([[
+--   command! -buffer -bar Run :!deno run %
+-- ]])
+
 vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>rp', ':Run<CR>', { noremap = true, expr = false, silent = true })
 
 vim.bo.shiftwidth = 2
--- ftplugin/typescript.lua
+++ ftplugin/typescript.lua
@@ -2,6 +2,10 @@ vim.cmd([[
   command! -buffer -bar Run :!ts-node %
 ]])
 
+-- vim.cmd([[
+--   command! -buffer -bar Run :!deno run %
+-- ]])
+
 vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>rp', ':Run<CR>', { noremap = true, expr = false, silent = true })
 
 vim.bo.shiftwidth = 2
