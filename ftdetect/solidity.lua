5b5ebd882b26d5368ab16cf4a9f76a06faab153d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-15 17:25:01 -0500

    :sparkles: Solidity

---
+++ ftdetect/solidity.lua
@@ -0,0 +1,3 @@
+vim.cmd([[
+  autocmd BufNewFile,BufRead *.sol setfiletype solidity
+]])
--- plugin/tressiter.lua
+++ plugin/tressiter.lua
@@ -9,3 +9,12 @@ require('nvim-treesitter.configs').setup({
     extended_mode = true,
   },
 })
+
+-- require "nvim-treesitter.parsers".get_parser_configs().Solidity = {
+--     install_info = {
+--       url = "https://github.com/JoranHonig/tree-sitter-solidity",
+--       files = {"src/parser.c"},
+--       requires_generate_from_grammar = true,
+--     },
+--     filetype = 'solidity'
+--   }
