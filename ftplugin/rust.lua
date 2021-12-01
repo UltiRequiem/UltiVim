3a1e073ffa4d6af293fd21cbdf13967490eff51a
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-12-01 11:31:48 -0500

    :recycle: Update

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -2,4 +2,10 @@ vim.cmd([[
   command! -buffer -bar Run :!rustc % -o main && ./main && rm main
 ]])
 
+vim.cmd([[
+  command! -buffer -bar RunTest :!rustc --test % -o main && ./main && rm main
+]])
+
+
 vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>rp', ':Run<CR>', { noremap = true, expr = false, silent = true })
+vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>rt', ':RunTest<CR>', { noremap = true, expr = false, silent = true })
