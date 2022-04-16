d38febe278ef98d36e30eaa4c0339ad0bd8211ce
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-16 11:45:34 -0500

    :recycle: Less mapping config

--- ftplugin/c.lua
+++ ftplugin/c.lua
@@ -2,7 +2,8 @@ vim.cmd([[
   command! -buffer -bar Run :!gcc % && ./a.out && rm ./a.out
 ]])
 
-vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>rp', ':Run<CR>', { noremap = true, expr = false, silent = true })
+
+vim.keymap.set("n","<leader>rp","<cmd>:Run<cr>",{silent=true, buffer=true})
 
 vim.bo.shiftwidth = 2
 vim.bo.tabstop = 2
--- plugin/mappings.lua
+++ plugin/mappings.lua
@@ -1,8 +1,3 @@
--- Mapping helper
-local mapper = function(mode, key, result)
-  vim.api.nvim_set_keymap(mode, key, result, { noremap = true, silent = true, expr = false })
-end
-
 -- Expressive Mapping helper
 local expressive_mapper = function(mode, key, result)
   vim.api.nvim_set_keymap(mode, key, result, { silent = true, expr = true })
@@ -17,64 +12,58 @@ end
 vim.g.mapleader = ' '
 
 -- Escape from insert mode
-mapper('i', '<C-c>', '<Esc>')
+vim.keymap.set('i', '<C-c>', '<Esc>')
 
--- Save for muggles
-mapper('n', '<C-s>', ':w<CR>')
+-- Duplicate Line
+vim.keymap.set('n', 'tt', ':t.<CR>')
 
--- Nice
-mapper('n', 'ZE', ':qa!<CR>')
+-- Toggle Numbers
+vim.keymap.set('n', '<leader>n', ':set nu! rnu!<CR>')
 
--- Duplitcate Line
-mapper('n', 'tt', ':t.<CR>')
-
---  Toggle Numbers
-mapper('n', '<leader>n', ':set nu! rnu!<CR>')
-
--- use ESC to turn off search highlighting
-mapper('n', '<Esc>', ':noh<CR>')
+-- Turn off search highlighting
+vim.keymap.set('n', '<Esc>', ':noh<CR>')
 
 -- Get out of the Terminal
-mapper('t', '<Esc>', '<C-\\><C-n>')
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 
 -- Resize with arrows
-mapper('n', '<C-Up>', ':resize -2<CR>')
-mapper('n', '<C-Down>', ':resize +2<CR>')
-mapper('n', '<C-Left>', ':vertical resize -2<CR>')
-mapper('n', '<C-Right>', ':vertical resize +2<CR>')
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>')
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>')
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>')
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>')
 
 -- Copy to OS clipboard.
-mapper('n', '<Leader>y', '"+y')
-mapper('v', '<Leader>y', '"+y')
-mapper('n', '<Leader>yy', '"+yy')
+vim.keymap.set('n', '<Leader>y', '"+y')
+vim.keymap.set('v', '<Leader>y', '"+y')
+vim.keymap.set('n', '<Leader>yy', '"+yy')
 
 -- Paste from OS clipboard
-mapper('n', '<Leader>p', '"+p')
-mapper('n', '<Leader>P', '"+P')
-mapper('v', '<Leader>p', '"+p')
-mapper('v', '<Leader>P', '"+P"`"`"')
+vim.keymap.set('n', '<Leader>p', '"+p')
+vim.keymap.set('n', '<Leader>P', '"+P')
+vim.keymap.set('v', '<Leader>p', '"+p')
+vim.keymap.set('v', '<Leader>P', '"+P"`"`"')
 
-mapper('n', 'J', 'mzJ`z')
+vim.keymap.set('n', 'J', 'mzJ`z')
 
 -- Plugins Mappings ↓
 
 -- Telescope
-mapper('n', '<C-F>', ':Telescope live_grep<CR>')
-mapper('n', '<C-P>', ':Telescope find_files<CR>')
+vim.keymap.set('n', '<C-F>', ':Telescope live_grep<CR>')
+vim.keymap.set('n', '<C-P>', ':Telescope find_files<CR>')
 
 -- Tree
-mapper('n', '<C-n>', ':NvimTreeToggle<CR>')
+vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>')
 
 -- Hop.nvim
-mapper('n', '<Leader>f', ':HopWord<CR>')
-mapper('n', '<Leader>o', ':HopPattern<CR>')
+vim.keymap.set('n', '<Leader>f', ':HopWord<CR>')
+vim.keymap.set('n', '<Leader>o', ':HopPattern<CR>')
 
 -- Switch Theme
-mapper('n', '<leader>mm', [[<Cmd>lua require('material.functions').toggle_style()<CR>]])
+vim.keymap.set('n', '<leader>mm', [[<Cmd>lua require('material.functions').toggle_style()<CR>]])
 
 -- Coc.nvim
-mapper('n', '<F12>', ':CocCommand terminal.Toggle<CR>')
-mapper('n', '<F3>', ':Format<CR>')
+vim.keymap.set('n', '<F12>', ':CocCommand terminal.Toggle<CR>')
+vim.keymap.set('n', '<F3>', ':Format<CR>')
 
 plug_mapper('n', '<leader>rn', '<Plug>(coc-rename)')
 
