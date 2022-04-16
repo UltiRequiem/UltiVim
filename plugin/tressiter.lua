53a3c9cc28dcabe4109c213c9681f15599ea3616
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-16 11:59:53 -0500

    :sparkles: Re activate tressiter rainbow

--- plugin/tressiter.lua
+++ plugin/tressiter.lua
@@ -4,16 +4,7 @@ require('nvim-treesitter.configs').setup({
   indent = { enable = true },
 
   rainbow = {
-    -- enable = true,
+    enable = true,
     extended_mode = true,
   },
 })
-
--- require "nvim-treesitter.parsers".get_parser_configs().Solidity = {
---     install_info = {
---       url = "https://github.com/JoranHonig/tree-sitter-solidity",
---       files = {"src/parser.c"},
---       requires_generate_from_grammar = true,
---     },
---     filetype = 'solidity'
---   }
--- plugin/utils.lua
+++
@@ -1,4 +0,0 @@
-function _G.check_back_space()
-  local col = vim.api.nvim_win_get_cursor(0)[2]
-  return (col == 0 or vim.api.nvim_get_current_line():sub(col, col):match('%s')) and true
-end
