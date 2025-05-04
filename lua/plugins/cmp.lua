7d8f83afb58a49955ace9d79a29af0b28076c02b
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2025-05-04 11:38:57 -0500

    chore: bump luasnips

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -9,6 +9,16 @@ local source_mapping = {
 	["vim-dadbod-completion"] = "[DB]",
 }
 
+local has_words_before = function()
+	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+		return false
+	end
+
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+
+	return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
+
 local config = function()
 	local cmp = require("cmp")
 	local lspkind = require("lspkind")
