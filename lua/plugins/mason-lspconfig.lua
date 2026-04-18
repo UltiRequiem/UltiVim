a23ba1e63cf4531254fe25a21f827ad0dbd763be
UltiRequiem <eliaz.bobadilladev@gmail.com>
2026-04-18 10:53:17 -0500

    tweaks: warnings

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,4 +1,10 @@
 local opts = {
+	-- Exclude formatter-only tools that Mason installs but shouldn't be started as LSP servers.
+	-- mason-lspconfig auto-enables all installed Mason packages via vim.lsp.enable(); formatters
+	-- like stylua have an lsp/ config in nvim-lspconfig that tries to run them with --lsp, which fails.
+	automatic_enable = {
+		exclude = { "stylua" },
+	},
 	ensure_installed = {
 		"angularls",
 		"awk_ls",
